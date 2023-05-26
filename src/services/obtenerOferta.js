98236d90b1d3c018f47717f821aa048f2c7ae33e
Ricky <rickynel2001@gmail.com>
2023-05-25 22:03:34 -0500

    servicios listos con variables de entorno

--- src/services/obtenerOferta.js
+++ src/services/obtenerOferta.js
@@ -1,8 +1,8 @@
 export const obtenerOferta = async (offerId) => {
-  const res = await fetch(`https://api.infojobs.net/api/7/offer/${offerId}`, {
+  const res = await fetch(`api/${offerId}`, {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Basic ${process.env}`
+      Authorization: `Basic ${import.meta.env.VITE_API_INFOJOBS}`
     }
   })
 
--- src/services/obtenerOfertas.js
+++ src/services/obtenerOfertas.js
@@ -1,19 +1,10 @@
 export const obtenerOfertas = async () => {
-  const res = await fetch('/offers.json')
-  // const res = await fetch('api/api/9/offer', {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Authorization: 'Basic ZjMyODFkM2Y1YzE0NDkwNjhjYjg0OWJmNGRiZGE1Yjc6REFZbTcwRzRBMWpUWjBSYUxaTDBSTWNjWjhzM2NTekFZSGt4SnlyWFBLUEFVUkZrRXQ='
-  //   }
-  // })
+  const res = await fetch('api/api/9/offer', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${import.meta.env.VITE_API_INFOJOBS}`
+    }
+  })
 
   return await res.json()
-  // .then(res => {
-  //   console.log(res)
-  //   return res.json()
-  // })
-  // .catch(err => {
-  //   console.log(err)
-  //   throw new Error('No se pudo obtener las ofertas')
-  // })
 }
