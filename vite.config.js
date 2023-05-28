5dc5298700e7be38013f199f6d691c7bbe9b62db
Ricky <rickynel2001@gmail.com>
2023-05-28 18:23:41 -0500

    ajuste de la url del proxy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.infojobs.net/',
+        target: 'https://api.infojobs.net/api/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
